2b4bafca15499c17793ce4b87d9bf40e82cd2304
Dan Tao <daniel.tao@gmail.com>
2014-03-19 09:24:24 -0700

    wrapped library to make it appropriate for browsers

--- namedArgs.js
+++ namedArgs.js
@@ -1,63 +1,67 @@
-function defineArgumentsProperty(name, factory) {
-  Object.defineProperty(Object.prototype, name, {
-    get: function() {
-      if (this.toString() !== '[object Arguments]') {
-        return;
+(function() {
+
+  function defineArgumentsProperty(name, factory) {
+    Object.defineProperty(Object.prototype, name, {
+      get: function() {
+        if (this.toString() !== '[object Arguments]') {
+          return;
+        }
+
+        return factory(this);
+      }
+    });
+  }
+
+  defineArgumentsProperty('named', function(args) {
+    var map  = getNameToIndexMap(args.callee);
+
+    return function named(name) {
+      if (arguments.length === 0) {
+        return convertIndicesToValues(args, map);
       }
 
-      return factory(this);
-    }
+      return args[map[name]];
+    };
   });
-}
 
-defineArgumentsProperty('named', function(args) {
-  var map  = getNameToIndexMap(args.callee);
+  defineArgumentsProperty('names', function(args) {
+    var fn = args.callee;
 
-  return function named(name) {
-    if (arguments.length === 0) {
-      return convertIndicesToValues(args, map);
+    return function names() {
+      return getArgumentNames(fn);
+    };
+  });
+
+  function getArgumentNames(fn) {
+    var match = fn.toString().match(/function\s*(?:[\$\w]*)\s*\(([^\)]*)\)/),
+        names = match[1].split(/\s*,\s*/);
+
+    return names.map(trim);
+  }
+
+  function getNameToIndexMap(fn) {
+    var names = getArgumentNames(fn),
+        map   = {};
+
+    for (var i = 0; i < names.length; ++i) {
+      map[names[i]] = i;
     }
 
-    return args[map[name]];
-  };
-});
-
-defineArgumentsProperty('names', function(args) {
-  var fn = args.callee;
-
-  return function names() {
-    return getArgumentNames(fn);
-  };
-});
-
-function getArgumentNames(fn) {
-  var match = fn.toString().match(/function\s*(?:[\$\w]*)\s*\(([^\)]*)\)/),
-      names = match[1].split(/\s*,\s*/);
-
-  return names.map(trim);
-}
-
-function getNameToIndexMap(fn) {
-  var names = getArgumentNames(fn),
-      map   = {};
-
-  for (var i = 0; i < names.length; ++i) {
-    map[names[i]] = i;
+    return map;
   }
 
-  return map;
-}
+  function convertIndicesToValues(args, nameToIndexMap) {
+    var map = nameToIndexMap;
 
-function convertIndicesToValues(args, nameToIndexMap) {
-  var map = nameToIndexMap;
+    for (var key in map) {
+      map[key] = args[map[key]];
+    }
 
-  for (var key in map) {
-    map[key] = args[map[key]];
+    return map;
   }
 
-  return map;
-}
+  function trim(string) {
+    return string.replace(/^\s+/, '').replace(/\s+$/, '');
+  }
 
-function trim(string) {
-  return string.replace(/^\s+/, '').replace(/\s+$/, '');
-}
+}());
